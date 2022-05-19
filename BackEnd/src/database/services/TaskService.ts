e3a6ec875519f11e247f235eb6937f91f332cdf8
aaguero96 <andre.luiz_1996@hotmail.com>
2022-05-19 11:34:02 -0300

    update: create a try catch

--- BackEnd/src/database/services/TaskService.ts
+++ BackEnd/src/database/services/TaskService.ts
@@ -2,7 +2,11 @@ import Task from '../models/TaskModel';
 
 export default class TaskService {
   getAll = async () => {
-    const tasks = await Task.findAll();
-    return tasks;
+    try {
+      const tasks = await Task.findAll();
+      return tasks;
+    } catch (e) {
+      console.error(e);
+    }
   };
 }
